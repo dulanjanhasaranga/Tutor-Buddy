89b28c352c09a7842d41cf09185a6d477489f130
Dulanjan Hasaranga <dulanjanhasaranga2004@gmail.com>
2026-08-13 01:36:50 +0530

    feat: add Booking.java

---
+++ src/main/java/com/tutor_buddy/model/Booking.java
@@ -0,0 +1,29 @@
+package com.tutor_buddy.model;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class Booking {
+    private Long id;
+    private Long userId;
+    private Long tutorId;
+    private String subject;
+    private LocalDateTime sessionDate;
+    private String startTime;
+    private String endTime;
+    private double duration; // in hours
+    private String paymentMethod;
+    private double total;
+    private String status; // "upcoming", "completed", "canceled"
+    private String specialRequests;
+
+    @Data
+    static class Availability {
+        private String day;
+        private String startTime;
+        private String endTime;
+    }
+
+}
